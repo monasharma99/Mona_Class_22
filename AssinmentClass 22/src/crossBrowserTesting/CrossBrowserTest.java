45c6d3fde896292cfe9e6768739c8532eaa02539
Deepinder <deepinder@whitecircleschool.com>
2018-11-14 09:06:23 -0500

    Comments on assignments and Validate Date and Time Format code

--- AssinmentClass 22/src/crossBrowserTesting/CrossBrowserTest.java
+++ AssinmentClass 22/src/crossBrowserTesting/CrossBrowserTest.java
@@ -11,46 +11,45 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-
-
 public class CrossBrowserTest {
 	WebDriver driver;
 	String expectedValue;
 	String actualValue;
-	
+
 	@BeforeTest
 	@Parameters("browser")
-	
+
 	public void openBrowser(String browser) {
 		if (browser.equals("Chrome")) {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");	
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 			driver = new ChromeDriver();
 		}
 
 		else if (browser.equals("Internet Explorer")) {
-			System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");	
-				driver = new InternetExplorerDriver();
-			}
-	
+			System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+
 		else if (browser.equals("Firefox")) {
-			System.setProperty("webdriver.FirefoxDriver.driver", "geckodriver.exe");	
-				driver = new FirefoxDriver();
-			}
-		
+			System.setProperty("webdriver.FirefoxDriver.driver", "geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
 		else if (browser.equals("Edge")) {
-			System.setProperty("webdriver.edge.driver", "MicrosoftWebDriver.exe");	
-				driver = new EdgeDriver();
-			}
+			System.setProperty("webdriver.edge.driver", "MicrosoftWebDriver.exe");
+			driver = new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get("https://www.linkedin.com/");
-	}	
-		
-	@Test (priority =0)
-	
-	public void VerifySignInButton() {
-	boolean signInButton =	driver.findElement(By.id("login-submit")).isEnabled();	
-	Assert.assertFalse(signInButton);
-	}
-	
 	}
 
+	@Test(priority = 0)
+
+	public void VerifySignInButton() {
+		boolean signInButton = driver.findElement(By.id("login-submit")).isEnabled();
+		Assert.assertFalse(signInButton);
+	}
+
+}
+
+//Good
--- AssinmentClass 22/src/faceBook/Verify3Methods.java
+++ AssinmentClass 22/src/faceBook/Verify3Methods.java
@@ -12,39 +12,45 @@ public class Verify3Methods {
 	WebDriver driver;
 	String expectedValue;
 	String actualValue;
-	
+
 	@BeforeTest
 	public void openBrowser() {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe" );	
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
-		
-		}
-	
-	@Test (priority=1)
+
+	}
+
+	@Test(priority = 1)
 	public void verifyText() {
 		expectedValue = "It’s free and always will be.";
-		actualValue = driver.findElement(By.cssSelector("#content > div > div > div > div > div._6_._74 > div.mbl._3m9._6o._6s._mf")).getText();
+		actualValue = driver
+				.findElement(
+						By.cssSelector("#content > div > div > div > div > div._6_._74 > div.mbl._3m9._6o._6s._mf"))
+				.getText();
 		Assert.assertEquals(actualValue, expectedValue);
-	
+
 	}
-	@Test (priority=2, enabled = false)
+
+	@Test(priority = 2, enabled = false)
 	public void verifyFirstNameVisibility() {
-	driver.findElement(By.xpath("//*[@id=\"u_0_9\"]")).sendKeys("Mona");
-	boolean	firstName = driver.findElement(By.xpath("//*[@id=\"u_0_9\"]")).isEnabled();
-	Assert.assertTrue(firstName);
-		
-	
+		driver.findElement(By.xpath("//*[@id=\"u_0_9\"]")).sendKeys("Mona");
+		boolean firstName = driver.findElement(By.xpath("//*[@id=\"u_0_9\"]")).isEnabled();
+		Assert.assertTrue(firstName);
+
 	}
-	@Test (priority=3)
+
+	@Test(priority = 3)
 	public void verifyErrorMessage() {
-	driver.findElement(By.id("loginbutton")).click();
-	boolean ErrorMessage = driver.findElement(By.cssSelector("#globalContainer > div.uiContextualLayerPositioner._572t.uiLayer > div > div > div")).isDisplayed();
-	Assert.assertTrue(ErrorMessage);
-		
-	}
+		driver.findElement(By.id("loginbutton")).click();
+		boolean ErrorMessage = driver
+				.findElement(By.cssSelector(
+						"#globalContainer > div.uiContextualLayerPositioner._572t.uiLayer > div > div > div"))
+				.isDisplayed();
+		Assert.assertTrue(ErrorMessage);
 
 	}
-	
 
+}
+//Good
