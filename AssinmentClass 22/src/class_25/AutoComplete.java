d2f35d3f83b88841abb02c4bf743c2fecc9dcfc8
mona <monasharma2097@gmail.com>
2018-11-19 20:28:08 -0500

    Class26 Auto Complete

---
+++ AssinmentClass 22/src/class_25/AutoComplete.java
@@ -0,0 +1,53 @@
+package class_25;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+	
+
+public class AutoComplete {
+WebDriver driver;
+String expectedValue;
+String actualValue;
+
+
+
+	@BeforeTest
+	public void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+		}
+	@Test
+	public void SearchIndia() throws InterruptedException {
+		driver.findElement(By.cssSelector("#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input")).sendKeys("India");
+		
+		Thread.sleep(2000);
+		
+		WebElement search = driver.findElement(By.cssSelector("#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input"));
+		Actions action = new Actions(driver);
+		action.click(search).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+	
+	}
+		 
+	
+		
+		
+		
+		
+		
+	}
+	
+
